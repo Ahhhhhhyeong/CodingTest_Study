ef14a83c80ec063796718bbaf1f96fcfc9f48b08
Ahhhhhhyeong <dkdud081@gmail.com>
2022-02-04 11:25:18 +0900

    0204 study

---
+++ Baekjoon/1단계/10998.js
@@ -0,0 +1,13 @@
+//import * as readline from 'node:readline';
+const readline = require("readline");
+ const std = readline.createInterface({
+     input: process.stdin,
+     output: process.stdout
+ });
+
+/* 문자형 숫자를 두개 입력받아 숫자로 변환한후 두개를 더해서 출력하는 예제  */
+std.on('line',(line)=>{
+    input = line.split(' ').map(el => Number(el));
+    console.log(input[0] * input[1]);
+    std.close();
+}).on('close',()=>process.exit())
