6eae432ca2c2f220c51fe90efdc2ac6bc2d85891
Ahhhhhhyeong <dkdud081@gmail.com>
2022-02-14 23:17:16 +0900

    4344 success!

--- Baekjoon/5단계/4344.js
+++ Baekjoon/5단계/4344.js
@@ -6,21 +6,25 @@
  const fs = require("fs");
  const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
  let input = fs.readFileSync(filePath).toString().split("\n");
- let arr = input.map((e) => {
-   return e.split(' ');
- }); 
 
- for(let i = 1; i < input.length; i++){
+
+ for(let i = 1; i <= input[0]; i++){
     let avr = 0, percent = 0; 
     for(let j = 1; j < arr[i].length; j++){
        avr += Number(arr[i][j]);          // 각 줄별 전체 합 구하기
     }
     avr = avr / Number(arr[i][0]);        // 각 줄별 평균
-    
-   //  for(let k = 1; k < arr[i].length; k++){
-       
-   //  }
-   
-    console.log(avr.toFixed(3));
+    let highScore = 0;
+    for(let k = 1; k < arr[i].length; k++){
+       if(avr < arr[i][k])
+         highScore++;
+    }
+    percent = (highScore / Number(arr[i][0]) * 100).toFixed(3);
+    console.log(percent + '%');
+    // 숏코딩 참고함
+   // let _split = input[i].split(' ').map(Number);
+   // let num = _split.shift();
+   // let avg = _split.reduce((x,y) => x+y,0)/num;
+   // console.log(String((_split.filter(x=>x>avg).length/num*100).toFixed(3)) + "%");
  }
  
