e8a1ff9f46a331f4823a889163ec4eea28b3b406
Ahhhhhhyeong <dkdud081@gmail.com>
2022-02-18 20:08:05 +0900

    문자열 반복

---
+++ Baekjoon/7단계/2675.js
@@ -0,0 +1,25 @@
+/**
+ * 문자열 S를 입력받은 후에, 각 문자를 R번 반복해 새 문자열 P를 만든 후 출력하는 프로그램을 작성하시오. 
+ * 즉, 첫 번째 문자를 R번 반복하고, 두 번째 문자를 R번 반복하는 식으로 P를 만들면 된다. 
+ * S에는 QR Code "alphanumeric" 문자만 들어있다.
+ * QR Code "alphanumeric" 문자는 0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ\$%*+-./: 이다.
+ */
+
+const fs = require("fs");
+ const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+ let input = fs.readFileSync(filePath).toString().split("\n");
+
+ 
+ for(let i = 1; i <= parseInt(input[0]); i++){
+    let split = input[i].split(' ');
+    let cnt = split[0];
+    let p = split[1].split('');
+    let msg = '';
+    for(let j = 0; j < p.length; j++){
+        msg += p[j].repeat(cnt);
+    }
+    console.log(msg);
+ }
+
+
+ 
