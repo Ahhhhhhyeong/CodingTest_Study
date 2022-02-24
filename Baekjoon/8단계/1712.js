8bba29652d80caeecff0864fbd51be4b5d29b73a
Ahhhhhhyeong <dkdud081@gmail.com>
2022-02-24 18:07:44 +0900

    손익분기점 RunTimeOut

--- Baekjoon/8단계/1712.js
+++ Baekjoon/8단계/1712.js
@@ -6,9 +6,26 @@
  const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
  let input = fs.readFileSync(filePath).toString().split(" ");
 
- if(Number(input[1]) >= Number(input[2])){
-     result = -1;
- } else{
-     result = Number(input[0]) / (Number(input[2]) - Number(input[1])) + 1;
+ let A = Number(input[0]);
+ let B = Number(input[1]);
+ let C = Number(input[2]);
+ let cnt = 0, money = 0;
+ let income = C - B;
+
+ while(1){
+    if( income === -1 ) {
+        console.log(-1);
+        break;
+    }
+    
+    if(A >= money){
+        money += income;
+        cnt++;
+        continue;
+    }
+    
+    if(A < money){
+        console.log(cnt);
+        break;
+    }
  }
- console.log(result);
