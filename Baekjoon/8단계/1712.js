e3d0349c3c13e97d9cf88a841d0cf1cb51e19dd5
Ahhhhhhyeong <dkdud081@gmail.com>
2022-02-24 18:58:10 +0900

    손익분기점 3차시도

--- Baekjoon/8단계/1712.js
+++ Baekjoon/8단계/1712.js
@@ -5,27 +5,9 @@
  const fs = require("fs");
  const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
  let input = fs.readFileSync(filePath).toString().split(" ");
-
- let A = Number(input[0]);
- let B = Number(input[1]);
- let C = Number(input[2]);
- let cnt = 0, money = 0;
- let income = C - B;
-
- while(1){
-    if( income === -1 ) {
-        console.log(-1);
-        break;
-    }
-    
-    if(A >= money){
-        money += income;
-        cnt++;
-        continue;
-    }
-    
-    if(A < money){
-        console.log(cnt);
-        break;
-    }
+ if(parseInt(input[1]) >= parseInt(input[2])){
+     result = -1;
+ } else{
+     result = Math.floor(parseInt(input[0]) / (parseInt(input[2]) - parseInt(input[1]))) + 1;
  }
+ console.log(result);
