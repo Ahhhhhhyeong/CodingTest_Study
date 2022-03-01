0515aa63455f95033f101aab5246dec335f887bb
Ahhhhhhyeong <dkdud081@gmail.com>
2022-03-02 00:16:50 +0900

    부녀회장 정하기 2차시도

--- Baekjoon/8단계/2775.js
+++ Baekjoon/8단계/2775.js
@@ -8,19 +8,22 @@ const T = input.shift();
 for(let i = 0; i < T; i++){
     const a = parseInt(input.shift());      // 층 수
     const b = parseInt(input.shift());      // 룸 넘버
+    ResidentNum(a,b);
+}
+
+function ResidentNum(floor, room){
     const apt = [];
-    for(let j = 0; j <= a; j++){        // 2차원 배열을 만듦
+    for(let j = 0; j <= floor; j++){        // 2차원 배열을 만듦
         apt.push([1]);
-        for(let k = 1; k <= b; k++){    
+        for(let k = 1; k <= room; k++){    
             if(j === 0){                // 0층 k룸 = k명
-                apt[j].push(k);
-            } else{
-                if(k === 1) apt[j].push(1);     // 1번룸 = 1명 
-                else apt[j].push(apt[j-1][k] + apt[j][k-1]);    // j층 k번룸 = (j-1층 k번룸인원 + j층 k-1번룸인원)
+               apt[j].push(k);
+               continue;
             }
+            apt[j].push(k===1 ? 1 : apt[j-1][k] + apt[j][k-1]);            
         }
     }
-    console.log(apt[a][b]);
+    console.log(apt[floor][room]);
 }
 
 
