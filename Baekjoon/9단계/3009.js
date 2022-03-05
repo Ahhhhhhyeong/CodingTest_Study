4745a169d8a0c1cd775c5fa4a8fb4eed2a434ebd
Ahhhhhhyeong <dkdud081@gmail.com>
2022-03-05 21:39:06 +0900

    직사각형 만들기 2차시도

--- Baekjoon/9단계/3009.js
+++ Baekjoon/9단계/3009.js
@@ -1,16 +1,9 @@
 /* 직사각형 만들기 */
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-let input = require("fs").readFileSync(filePath).toString().trim().split('\r\n');
-
-let arrX = [];
-let arrY = [];
-for(let i = 0; i < 3; i++){
-    arrX.push(Number(input[i].split(' ')[0]));
-    arrY.push(Number(input[i].split(' ')[1]));
+let input = require("fs").readFileSync(filePath).toString().trim().split(/\s+/).map(Number);
+let arrX = input.filter((v,i)=>i%2===0);
+let arrY = input.filter((v,i)=>i%2!=0);
+console.log(getVertex(arrX), getVertex(arrY));
+function getVertex(arr){
+    return arr[0] === arr[1] ? arr[2] : (arr[1]===arr[2]?arr[0]:arr[1]);
 }
-arrX = arrX.sort();
-arrY = arrY.sort();
-
-let x = arrX[1] === arrX[0] ? arrX[2] : arrX[0];
-let y = arrY[1] === arrY[0] ? arrY[2] : arrY[0];
-console.log(`${x} ${y}`);
