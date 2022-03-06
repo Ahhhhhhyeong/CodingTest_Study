052978b229d82df977df9e42e14de9d0783b4e4e
Ahhhhhhyeong <dkdud081@gmail.com>
2022-03-06 21:07:46 +0900

    직각삼각형 7차시도...

--- Baekjoon/9단계/4153.js
+++ Baekjoon/9단계/4153.js
@@ -1,19 +1,12 @@
 /** 직각삼각형 */
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-let input = fs.readFileSync(filePath).toString().split('\r\n');
-
-// for(let i = 0; i < input.length-1; i++){
-//     let arr = input[i].split(' ').map(Number);
-//     arr = arr.sort(function(a, b){ return b - a });
-//     console.log((arr[1]*arr[1])+(arr[2]*arr[2]) === (arr[0]*arr[0]) ? "right" : "wrong");
-// }
-let i = 0;
-while(1){
-    let arr = input[i].split(' ').map(Number);
-    if(arr[0] === 0 && arr[1] === 0 && arr[2] === 0) break;
-
-    arr = arr.sort(function(a, b) { return b - a });
-    console.log((arr[1]*arr[1])+(arr[2]*arr[2]) === (arr[0]*arr[0]) ? "right" : "wrong");
-    i++;
+let input = fs.readFileSync(filePath)
+              .toString()
+              .trim()
+              .split('\n')
+              .map(x => x.split(' ').map(Number));
+for(let i = 0; i < input.length - 1; i++){
+    let arr = input[i].sort((a,b) => a - b);
+    console.log(arr[0]**2 + arr[1]**2 === arr[2]**2 ? "right" : "wrong");
 }
