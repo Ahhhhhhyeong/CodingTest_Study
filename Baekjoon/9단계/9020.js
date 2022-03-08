88dbc7abf0a6506b03a2c0f07adebfdda58384c3
Ahhhhhhyeong <dkdud081@gmail.com>
2022-03-08 23:09:22 +0900

    골드바흐의 추측_에라토스의 체활용

---
+++ Baekjoon/9단계/9020.js
@@ -0,0 +1,22 @@
+/** 골드바흐의 추측  */
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().trim().split('\n').map(Number);
+
+
+
+
+let solution = (n) => {
+    let arr = Array(n + 1).fill(true).fill(false, 0, 2);
+
+    for(let i = 2; i < Number(n**0.5) + 1; i++){
+        if(arr[i]){
+            for(let j = i * i; j <= n; j += i){
+                arr[j] = false;
+            }
+        }
+    }
+    return arr[arr.length-1];
+}  
+
+
