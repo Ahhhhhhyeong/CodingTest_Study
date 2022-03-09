37979f13a447211babee55a50e50863d3562abd3
Ahhhhhhyeong <dkdud081@gmail.com>
2022-03-09 23:00:41 +0900

    소수 구하기

--- Baekjoon/9단계/1929.js
+++ Baekjoon/9단계/1929.js
@@ -2,21 +2,25 @@
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(filePath).toString().trim().split('\n');
-let x = input[0].split(' ').map(Number);
-console.log(x);
-//let arrPrime = [];
-let solution = (n) => {
-    let arr = Array(n + 1).fill(true).fill(false, 0, 2);
 
-    for(let i = 2; i < Number(n**0.5) + 1; i++){
-        if(arr[i]){
-            for(let j = i * i; j <= n; j += i){
-                arr[j] = false;
-            }
-        }
+const strToNumArr = input[0].split(' ').map(Number);
+const [M, N] = strToNumArr;
+const isPrimeNumber = Array(N + 1).fill(true);
+isPrimeNumber[1] = false;
+
+for(let n = 2; n <= Math.ceil(Math.sqrt(N)); n++){
+    if(isPrimeNumber[n]){
+        let m = 2;
+         while(n * m <= N){
+             isPrimeNumber[n*m] = false;
+             m++;
+         }
     }
-    return arr;
-}  
-for(let i = x[0]; i <= x[1]; i++){
-    solution(i);
 }
+const results = [];
+for(let n = M; n <= N; n++){
+    if(isPrimeNumber[n]){
+        results.push(n);
+    }
+}
+console.log(results.join('\n'));
