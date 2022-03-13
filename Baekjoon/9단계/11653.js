28ae7dbfe97f0c46f1afc04b17269d50ed028ade
Ahhhhhhyeong <dkdud081@gmail.com>
2022-03-13 14:51:00 +0900

    소인수분해_시간초과

--- Baekjoon/9단계/11653.js
+++ Baekjoon/9단계/11653.js
@@ -2,21 +2,12 @@
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(filePath).toString().trim().split('\n').map(Number);
-let originNum = input;
 let primeNumber = 2;
-let smallFactorization = [];
-
-// for(let i = 2; i <= input; i++){
-//     while(input % i === 0){
-//         input /= i;
-//         console.log(i);
-//     }
-// }
 
 while(input != 1){
-    if(inptu % primeNumber === 0){
+    if(input % primeNumber === 0){
         console.log(primeNumber);
-        n /= primeNumber;
+        input /= primeNumber;
     }else{
         primeNumber++;
     }
