eaf96cae0dbc02a7a024b676dc4e39ff09428b80
Ahhhhhhyeong <dkdud081@gmail.com>
2022-03-13 14:45:04 +0900

    소인수분해 런타임에러(미해결)

--- Baekjoon/9단계/11653.js
+++ Baekjoon/9단계/11653.js
@@ -2,10 +2,22 @@
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(filePath).toString().trim().split('\n').map(Number);
+let originNum = input;
+let primeNumber = 2;
+let smallFactorization = [];
 
-for(let i = 2; i <= input; i++){
-    while(input % i === 0){
-        input /= i;
-        console.log(i);
+// for(let i = 2; i <= input; i++){
+//     while(input % i === 0){
+//         input /= i;
+//         console.log(i);
+//     }
+// }
+
+while(input != 1){
+    if(inptu % primeNumber === 0){
+        console.log(primeNumber);
+        n /= primeNumber;
+    }else{
+        primeNumber++;
     }
 }
