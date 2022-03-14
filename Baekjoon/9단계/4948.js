2f892de220e3a2ee3387a80ccee8e16babab9d00
Ahhhhhhyeong <dkdud081@gmail.com>
2022-03-14 22:45:51 +0900

    베르트랑 공준(N에서 2N까지의 소수개수구하기)

---
+++ Baekjoon/9단계/4948.js
@@ -0,0 +1,40 @@
+/** 베르트랑 공준 */
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().trim().split('\n').map(Number);
+let i = 0;
+
+const isPrime = (N, M) => {
+    const isPrimeNumber = Array(M + 1).fill(true);
+    isPrimeNumber[1] = false;
+
+    for(let n = 2; n <= Math.ceil(Math.sqrt(M)); n++){
+        if(isPrimeNumber[n]){
+            let m = 2;
+            while(n * m <= M){
+                isPrimeNumber[n*m] = false;
+                m++;
+            }
+        }
+    }
+    const results = [];
+    for(let n = N; n <= M; n++){
+        if(isPrimeNumber[n]){
+            results.push(n);
+        }
+    }
+    return results;
+}
+
+while(input[i] != 0){
+    let n = input[i] + 1;
+    let n2 = input[i] * 2;
+    if(input[i] === 1){
+        console.log(1);
+        i++;
+        continue;
+    }
+    let arr = isPrime(n, n2);
+    console.log(arr.length);
+    i++;
+}
