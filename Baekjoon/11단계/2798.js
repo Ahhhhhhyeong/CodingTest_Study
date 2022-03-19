d97fd0d31009d291e4027f9960cf640a204b38ee
Ahhhhhhyeong <dkdud081@gmail.com>
2022-03-19 23:59:45 +0900

    블랙잭_3차시도_clear

--- Baekjoon/11단계/2798.js
+++ Baekjoon/11단계/2798.js
@@ -4,15 +4,13 @@ const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(filePath).toString().trim().split('\n');
 const [N, M] = input[0].split(" ").map(Number); // N은 카드개수 M은 기준 숫자(3장의 합)
 const cards = input[1].split(" ").map(Number);  // 카드에 쓰여진 숫자들 
-let result = M;
+let result = 0;
 
 for(let i = 0; i < N; i++){
     for(let j = i+1; j < N; j++){
         for(let k = j + 1; k < N; k++){
-            let sum = cards[i] + cards[j] + cards[k];
-            let gap = Math.abs(M - sum);
-            if(gap <= 0 || result >= sum){
-                result = sum;
+            if(cards[i]+cards[j]+cards[k] <= M && cards[i]+cards[j]+cards[k] > result) {
+                result = cards[i]+cards[j]+cards[k];
             }
         }
     }
