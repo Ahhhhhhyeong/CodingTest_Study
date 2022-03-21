867e5820f1c03618950e6d676b98ce99ae5950a8
Ahhhhhhyeong <dkdud081@gmail.com>
2022-03-21 21:02:42 +0900

    체스판 칠하기

---
+++ Baekjoon/11단계/1018.js
@@ -0,0 +1,57 @@
+/** 체스판 다시 칠하기 */
+const fs = require("fs");
+const { resourceLimits } = require("worker_threads");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().trim().split('\n');
+const [N, M] = input.shift().split(' ').map(Number);
+
+let whiteFirst = [
+    "WBWBWBWB", 
+    "BWBWBWBW",
+    "WBWBWBWB", 
+    "BWBWBWBW",
+    "WBWBWBWB", 
+    "BWBWBWBW",
+    "WBWBWBWB", 
+    "BWBWBWBW"
+];
+let blackFirst = [
+    "BWBWBWBW",
+    "WBWBWBWB",
+    "BWBWBWBW",
+    "WBWBWBWB",
+    "BWBWBWBW",
+    "WBWBWBWB",
+    "BWBWBWBW",
+    "WBWBWBWB"
+];
+
+let min = 64;
+
+const whiteFirstArr = (y, x) => {
+    let counter = 0;
+    for(let i = y; i < y+8; i++){
+        for(let j = x; j < x + 8; j++){
+            if(input[i][j] !== whiteFirst[i - y][j - x]) counter++;
+        }
+    }
+    return counter;
+}
+
+const BlackFirstArr = (y, x) => {
+    let counter = 0;
+    for(let i = y; i < y + 8; i++){
+        for(let j = x; j < x + 8; j++){
+            if(input[i][j] !== blackFirst[i - y][j - x]) counter++;
+        }
+    }
+    return counter;
+}
+let minArr = [];
+for(let i = 0; i + 7 < N; i++){
+    for(let j = 0; j + 7 < M; j++){
+        minArr.push(whiteFirstArr(i, j));
+        minArr.push(BlackFirstArr(i, j));
+    }
+}
+console.log(Math.min.apply(null, minArr));
