4d233c072db60fe0671bdfcdeee320fe76c8bf6a
Ahhhhhhyeong <dkdud081@gmail.com>
2022-03-23 21:15:02 +0900

    수정렬하기

---
+++ Baekjoon/12단계/2750.js
@@ -0,0 +1,6 @@
+/** 수 정렬하기 */
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().trim().split('\n').map(Number);
+input.shift(); // 첫번째 숫자는 N개 표현이라 제거하여줌
+console.log(input.sort((a,b) => a-b).join('\n'));
