45c43bc6e4439e82c1523b0e24dd2fe886ac288d
Ahhhhhhyeong <dkdud081@gmail.com>
2022-03-24 22:04:03 +0900

    산술평균, 중앙값, 최빈값, 범위 구하기_1차 시도

---
+++ Baekjoon/12단계/2108.js
@@ -0,0 +1,43 @@
+/** 통계학
+ * 산술평균, 중앙값, 최빈값, 범위 구하기
+ */
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().trim().split('\n').map(Number);
+const countNum = input.shift();
+const sortArr = input.sort((a,b) => a - b);
+
+// 최빈값 구하기
+const frequentValue = (x) => {
+    const numMap = {};
+    for(let num of x){
+        if(numMap[num]){
+            numMap[num] = numMap[num] + 1;
+        } else {
+            numMap[num] = 1;
+        }
+    }       
+    let frequentNum = Math.max.apply(null, Object.values(numMap));
+    let frequentArr =[];
+    let frequentResult = 0;
+    for(let numKey in numMap){
+        if(numMap[numKey] === frequentNum)
+            frequentArr.push(numKey);
+    }
+    frequentArr = frequentArr.sort((a,b) => a - b);  
+    frequentResult = frequentArr.length > 1 ? frequentArr[1] : frequentArr[0];
+    return frequentResult;
+}
+const avg = (x) =>{
+    let result = 0;
+    for(let i = 0; i < x; i++){
+        result += input[i];
+    }
+    result = Math.round(result / x);
+    return result;
+}
+console.log(avg(countNum)); // 산술평균
+console.log(sortArr[Math.round(countNum / 2) - 1]); // 중앙값
+console.log(frequentValue(sortArr)); // 최빈값
+console.log(sortArr[sortArr.length - 1] - sortArr[0]); // 범위
+
