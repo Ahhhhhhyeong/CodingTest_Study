768f87e40b0add04b78cd2f497ca6daa821209a9
Ahhhhhhyeong <dkdud081@gmail.com>
2022-03-25 23:46:29 +0900

    좌표정렬하기(11650번, 11651번 함께 풀이함)

--- Baekjoon/12단계/11650.js
+++ Baekjoon/12단계/11650.js
@@ -1,16 +1,38 @@
-/** 좌표 정렬하기 */
+/** 좌표 정렬하기 
+ * 11650번, 11651번 
+ * 11650 : x좌표가 증가하는 순으로, x좌표가 같으면 y좌표가 증가하는 순서로
+ * 11651 : y좌표가 증가하는 순으로, y좌표가 같으면 x좌표가 증가하는 순서로
+*/
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(filePath).toString().trim().split('\n');
-const cntNum = input.shift();
+const N = input.shift();
 const cordArr = input.map(cord => cord.split(' ').map(Number));
-let results = ''; 
-cordArr.sort((a, b) => {
-    if(a[0] !== b[0]){
+
+const cordArrSortAtoB = (arr) => { // 11650
+    let results = ''; 
+    arr.sort((a,b) => {
+        if(a[0] !== b[0]){
+            return a[0] - b[0];
+        }
+        return a[1] - b[1];
+    }).forEach(cord => {
+        results += `${cord[0]} ${cord[1]}\n`;
+    });
+    return arr;
+}
+
+const cordArrSortBtoA = (arr) => { // 11651
+    let results = '';
+    arr.sort((a,b) =>{
+        if(a[1] !== b[1]) 
+            return a[1] - b[1];
         return a[0] - b[0];
-    }
-    return a[1] - b[1];
-}).forEach(cord => {    
-    results += `${cord[0]} ${cord[1]}\n`;
-});
-console.log(results);
+    }).forEach( cord => {
+        results += `${cord[0]} ${cord[1]}\n`
+    });
+    return results;
+}
+
+console.log(cordArrSortAtoB(cordArr)); // 11650번
+console.log(cordArrSortBtoA(cordArr)); // 11651번
