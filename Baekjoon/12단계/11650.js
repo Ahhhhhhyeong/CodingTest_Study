1f6297587fbb1b6390073ac0559b205a7ff88ea2
Ahhhhhhyeong <dkdud081@gmail.com>
2022-03-25 23:36:16 +0900

    좌표 정렬하기_2차시도

--- Baekjoon/12단계/11650.js
+++ Baekjoon/12단계/11650.js
@@ -4,14 +4,13 @@ const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(filePath).toString().trim().split('\n');
 const cntNum = input.shift();
 const cordArr = input.map(cord => cord.split(' ').map(Number));
-let results = []; let i = 0;
+let results = ''; 
 cordArr.sort((a, b) => {
     if(a[0] !== b[0]){
         return a[0] - b[0];
     }
     return a[1] - b[1];
 }).forEach(cord => {    
-    results[i] = `${cord[0]} ${cord[1]}`;
-    i++;
+    results += `${cord[0]} ${cord[1]}\n`;
 });
-console.log(results.forEach(element => console.log(element)));
+console.log(results);
