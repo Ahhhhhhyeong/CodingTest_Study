2180b04ab9d92c5d304a598078961048b85adcd7
Ahhhhhhyeong <dkdud081@gmail.com>
2022-03-28 20:50:21 +0900

    나이순 정렬_(중간저장)

---
+++ Baekjoon/12단계/10814.js
@@ -0,0 +1,6 @@
+/** 나이순 정렬 */
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().trim().split('\n');
+
+const N = input.shift();
