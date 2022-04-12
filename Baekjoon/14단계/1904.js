9bbf04fd1d42411f7290b50bc8187335cba892c6
unknown <dkdud081@gmail.com>
2022-04-12 17:41:18 +0900

    01 타일_4차시도

--- Baekjoon/14단계/1904.js
+++ Baekjoon/14단계/1904.js
@@ -2,16 +2,15 @@ const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(filePath).toString().trim().split('\n');
 const N = parseInt(input[0]);
-const memo = [...new Array(N + 1)].fill(-1);
-memo[0] = 0;
+const memo = Array.from({length: N}, () => 0);
+
 memo[1] = 1;
 memo[2] = 2;
+memo[3] = 3;
 
-const tile = (n) => {
-    if(memo[n] === -1){
-        memo[n] = (tile(n-1) + tile(n-2)) % 15746;
-    }
-    return memo[n];    
+for(let i = 4; i <= N; i++){
+    memo[i] = (memo[i - 2] + memo[i - 1]) % 15746;
 }
 
-console.log(tile(N));
+
+console.log(memo[N]);
