8e0b8d88c5f3d3af4f4f365e91702bc61bb1c288
unknown <dkdud081@gmail.com>
2022-04-14 18:07:46 +0900

    가장 긴 바이토닉 부분 수열(바이토닉수열 이해부족_미해결)

--- Baekjoon/14단계/11054.js
+++ Baekjoon/14단계/11054.js
@@ -5,31 +5,38 @@ let input = fs.readFileSync(filePath).toString().trim().split('\n');
 
 const n = input[0];
 const arr = input[1].split(' ').map(Number);
+const arr2 = [...arr];
 
-const arrCheck = (nums, n) => {
+const leftArrCheck = (arr, n) => { // 좌측부터의 LIS 구하기
+    const temp = [...arr];
     let i = 0;
-    while(i + 1 < n && nums[i] < nums[i+1]) ++i;
-    console.log(i);
-    if(i === 0 || i === n - 1)return false;
-    while(i + 1 < n && nums[i] > nums[i+1]) ++i;
-    if(i !== n - 1) return false;
+    while(i + 1 < n && arr[i] < arr[i+1]) ++i;
+    if(temp == arr) return false;
     return true;
 }
 
-console.log(arrCheck(arr, n));
-const dp = [];
-let max = 1;
-if(arrCheck(arr, n)){
-    for(let i = 0; i < n; i++){
-        dp[i] = 1;
-        for(let j = 0; j < i; j++){
-            if(arr[j] < arr[i] && dp[j] + 1 > dp[i]){
-                dp[i] = dp[j] + 1;
-                if(max < dp[i]){
-                    max = dp[i];
-                }
-            }        
+const rightArrCheck = (arr, n) => {
+    const temp = [...arr];
+    for(let i = n-1; i < 0; i--){
+        if(arr[i] < arr[i-1]){
+            arr[i] = arr[i-1];
         }
     }
-    console.log(max);
+    if(temp == arr) return false;
+    return true;
 }
+console.log(leftArrCheck(arr, n));
+console.log(rightArrCheck(arr2, n));
+console.log(arr, arr2);
+// const dp = [];
+// let max = 1;
+// if(leftArrCheck(arr, n) && rightArrCheck(arr2, n)){
+//     for(let i = 0; i < n; i++){
+//         dp[i] = 1;
+        
+//     }
+// }
+
+
+// 좌/우측의 각각의 값을 더하여 -1을 한 값 중 최대값을 구한다.
+// 해당위치가 2번 중복으로 -1을 해줌
