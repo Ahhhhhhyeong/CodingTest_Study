32ca3c70900f5f381839266b7da80c6da3334354
Ahhhhhhyeong <dkdud081@gmail.com>
2022-10-03 22:31:45 +0900

    정수 삼각형_fin

--- Baekjoon/14단계/1932.js
+++ Baekjoon/14단계/1932.js
@@ -1,12 +1,21 @@
 // 정수 삼각형
 const fs = require("fs");
+const { maxHeaderSize } = require("http");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(filePath).toString().trim().split('\n');
-const N = input.shift(); // 크기
 
-const dp = [];
-for(let i = 0; i < N; i++){
-    dp.push(input[i].split(" ").map(v => v));
+[N, ...arr] = input;
+N = Number(N);
+arr = arr.map(i => i.split(' ').map(i => Number(i)));
+solution(N, arr);
+
+function solution(N, arr) {
+    for(let i = N - 2; i >= 0; i--){
+        for(let j = 0; j < arr[i].length; j++){
+            arr[i][j] += Math.max(arr[i + 1][j], arr[i + 1][j + 1]);
+        }
+    }
+    console.log(arr[0][0]);
 }
 
-console.log(dp);
+//1932	맞았습니다!!	14632 kb 	216 ms
