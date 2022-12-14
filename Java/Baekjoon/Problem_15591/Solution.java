8b3be74ae9caed2352ec2e556f5e0e8f744e87e2
Ahhhhhhyeong <dkdud081@gmail.com>
2022-12-14 15:57:39 +0900

    MooTube(Spanning Tree, 신장트리 문제)

---
+++ Java/Baekjoon/Problem_15591/Solution.java
@@ -0,0 +1,55 @@
+package Java.Baekjoon.Problem_15591;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Scanner;
+
+/**
+ * MooTube (Silver)
+ */
+public class Solution {
+
+    public static void main(String[] args) throws Exception {
+        Scanner br = new Scanner(System.in);
+        int N = br.nextInt(); //동영상 개수
+        int Q = br.nextInt(); //질문 개수
+
+       List<int[]>[] video = new ArrayList[N+1];
+       
+       for(int i=1; i<=N; i++){
+         video[i] = new ArrayList<>();
+       }
+
+       for(int i=1; i<N; i++){
+         int p = br.nextInt(), q = br.nextInt(), r = br.nextInt();
+         video[p].add(new int[]{q, r});
+         video[q].add(new int[]{p, r});
+       }
+
+       StringBuilder sb = new StringBuilder();
+       for(int i=0; i<Q; i++){
+        int k = br.nextInt(), v = br.nextInt();
+
+        boolean[] visited = new boolean[N + 1];
+        visited[v] = true;
+        Queue<Integer> que = new LinkedList<>();
+        que.add(v);
+
+        int cnt = 0;
+        while(!que.isEmpty()){
+            int cur = que.poll();
+            for(int[] a : video[cur]){
+                if(!visited[a[0]] && a[1] >= k){
+                    que.add(a[0]);
+                    visited[a[0]] = true;
+                    cnt++;
+                }
+            }
+        }
+        sb.append(cnt).append('\n');
+       }
+       System.out.println(sb.toString());
+    }  
+}
