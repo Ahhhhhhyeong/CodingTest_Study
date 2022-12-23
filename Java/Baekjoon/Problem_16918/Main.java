5bfa4727258f526ed4de66882fe4dd83f4aaf889
Ahhhhhhyeong <dkdud081@gmail.com>
2022-12-23 23:24:58 +0900

    봄버맨 - solved!!

--- Java/Baekjoon/Problem_16918/Main.java
+++ Java/Baekjoon/Problem_16918/Main.java
@@ -1,99 +1,138 @@
 package Java.Baekjoon.Problem_16918;
-
+/**
+ * BOJ 16918 봄버맨
+ * 
+ * 유튜브 문제 풀이 참고함
+ * https://youtu.be/fFg5VXpsIxk
+ * https://youtu.be/XsrxGopW5-A
+ * 
+ */
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int R=0, C=0;
-    public static void main(String[] args) throws IOException{
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in)); //선언
-       
-        int N=0;
+    static int R=0, C=0, N=0;
+    static int[][] map;
+
+    static int[] di = {1, -1, 0, 0}; //상, 하, 좌, 우
+    static int[] dj = {0, 0, 1, -1};
+
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void solution() throws IOException{
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in)); //선언   
         StringTokenizer st = new StringTokenizer(bf.readLine());  
+
         R = Integer.parseInt(st.nextToken());     
         C = Integer.parseInt(st.nextToken());
         N = Integer.parseInt(st.nextToken()); 
 
-        char[][] arr = new char[R][C];
-        boolean[][] chk = new boolean[R][C];
+        map = new int[R][C];
+
         for(int i=0; i<R; i++){
             String str = bf.readLine();
             for(int j=0; j<C; j++){
-                arr[i][j] = str.toCharArray()[j];
-                if(arr[i][j]=='O') chk[i][j]=true;
+                if(str.charAt(j) == 'O') map[i][j] = 3;
+                else map[i][j] = 0;
             }
         }
 
-        if(N%2==0){ //짝수는 모두 o
-            for(int i=0; i<R; i++){
-                for(int j=0; j<C; j++){
-                    System.out.print("O");
-                }
-                System.out.println();
-            }
-        }
-        else{ //홀수인경우
-            int n = bfs(N);
-            if(n == 5){
-                result(arr);
+        if(0==N) return;
+
+        secondPassed();
+        --N;
+
+        if(0==N) return;
+        
+        boolean BombsOrPassed = true;
+
+        for(int s=0; s<N; s++){
+            if(BombsOrPassed){
+                secondPassed();
+                setupBooms();
+                explodeBomb();
             }
             else{
-                result(solution(arr,chk));
+                secondPassed();
+                explodeBomb();
             }
-        }
+        }       
 
+        bf.close();
+        return;
     }
 
-    public static int bfs(int N) {
-        int result = 0;
-        if(N==3 || N==5) return N;
-        while(N>2){
-            int tmp = N-4;
-            if(tmp == 3 || tmp == 5){
-                result = tmp;
-                break;
-            }
-            else result = tmp;
-        }
-        return result;
-    }
 
-    public static void result(char[][] arr){
+    public static void secondPassed() {
         for(int i=0; i<R; i++){
             for(int j=0; j<C; j++){
-                System.out.print(arr[i][j]);
-            }
-            System.out.println();
-        }
-    }
-
-    public static char[][] solution(char[][] arr, boolean[][] chk){
-        int[] xboom = {-1, 0, 1, 0}; //왼쪽, 위, 오른쪽, 아래
-        int[] yboom = {0, 1, 0, -1}; //왼쪽, 위, 오른쪽, 아래        
-        for(int i=0; i<R; i++){
-            for(int j=0; j<C; j++){
-                if(arr[i][j] == '.' && !chk[i][j]) {
-                    arr[i][j] = 'O';
-                    continue;
+                if(map[i][j] > 0){
+                    --map[i][j];
+                    if(map[i][j]==0) map[i][j] = -1;
                 }
-                if(arr[i][j]=='O'){
-                    chk[i][j] = true;
-                    arr[i][j] = '.';
-                    for(int k=0; k<4; k++){ //상하좌우 변경
-                        int x = i+xboom[k];
-                        int y = j+yboom[k];
-                        if(x>=0 && y>=0 && x<R && y<C){
-                            if(arr[x][y] == 'O' && chk[x][y]) continue;                          
-                            arr[x][y] = '.';
-                            chk[x][y] = true;
-                        }
-                    }
-                }                
             }
         }
-        return arr;
+    }
+
+    public static void setupBooms(){
+        for(int i=0; i<R; i++){
+            for(int j=0; j<C; j++){
+                if(map[i][j]==0) map[i][j] = 3;
+            }
+        }
+    }
+
+    public static void explodeBomb() {
+        Deque<int[]> que = new ArrayDeque<>();
+
+        for(int i=0; i<R; i++){
+            for(int j=0; j<C; j++){
+                if(map[i][j] < 0){
+                    map[i][j] = 0;
+                    que.add(new int[]{i, j});
+                }
+            }
+        }
+
+        for(int[] yx : que){
+            explodeBomb(yx[0], yx[1]);
+        }
+
+    }
+
+    public static void explodeBomb(int y, int x){
+        map[y][x] = 0;
+        for(int idx = 0; idx < di.length; idx++){
+            int cy = y - di[idx];
+            int cx = x - dj[idx];
+            if(IsInMap(cy, cx)) map[cy][cx] = 0;
+        }
+    }
+
+    private static boolean IsInMap(int y, int x) {
+        return ((0<=y) && (0<=x) && (y<R) && (x<C));
+    }
+
+    public static void printMap() throws IOException{
+        for(int i=0; i<R; i++){
+            for(int j=0; j<C; j++){
+                if(map[i][j] > 0) bw.write('O');
+                else bw.write('.');
+            }
+            bw.newLine();
+        }
+        bw.flush();
+        bw.close();
+    }
+
+    public static void main(String[] args) throws IOException{
+        solution();
+        printMap();
     }
 
 }
