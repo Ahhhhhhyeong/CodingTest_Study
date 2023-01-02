d46f23975ebce584dbe10b93daf98ebc2ec2dbf1
Ahhhhhhyeong <dkdud081@gmail.com>
2023-01-02 22:36:33 +0900

    2164 - 메모리초과(??)

--- Java/Baekjoon/Problem_2164/Main.java
+++ Java/Baekjoon/Problem_2164/Main.java
@@ -7,24 +7,18 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class Main {
-    static int N;
-    
     public static void main(String[] args) throws IOException{
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in)); //선언   
-        
-        N = Integer.parseInt(bf.readLine());
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in)); 
+        int N = Integer.parseInt(bf.readLine());
         Queue<Integer> cards = new LinkedList<>();
         for(int i=1; i<=N; i++){
             cards.offer(i);
-        }
-        
+        }        
         cards.poll();
-        while(cards.size() > 1){
+        while(cards.size() != 1){
             cards.offer(cards.poll());
             cards.poll();
         }
         System.out.println(cards.peek());
-    }
-
-    
+    }    
 }
