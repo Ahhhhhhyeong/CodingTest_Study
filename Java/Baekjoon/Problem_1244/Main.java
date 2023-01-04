ccdc32fd3d9d240d977ebfe45d760c5a36d299ce
Ahhhhhhyeong <dkdud081@gmail.com>
2023-01-04 20:51:13 +0900

    1244_start

---
+++ Java/Baekjoon/Problem_1244/Main.java
@@ -0,0 +1,59 @@
+package Java.Baekjoon.Problem_1244;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int sn, stn;
+    static int[] button;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in)); // 선언
+        sn = Integer.parseInt(bf.readLine());
+
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        button = new int[sn + 1];
+        for (int i = 1; i <= sn; i++) {
+            button[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < stn; i++) {
+            st = new StringTokenizer(bf.readLine());
+            int sex = Integer.parseInt(st.nextToken());
+            int num = Integer.parseInt(st.nextToken());
+            if (sex == 1)
+                ManSwitch(num);
+            else
+                GirlSwitch(num);
+        }
+
+        for (int i = 1; i <= sn; i++) {
+            System.out.printf("%d ", button[i]);
+        }
+    }
+
+    public static void ManSwitch(int num) {
+        for (int i = num; i <= sn; i += num) {
+            button[i] = button[i] == 1 ? 0 : 1;
+        }
+    }
+
+    public static void GirlSwitch(int num) {
+        int j = 0;
+        for (int i = num; i >= 1; i--) {
+            int tmp = i - j;
+            j++;
+            if (tmp == i) {
+                button[i] = button[i] == 1 ? 0 : 1;
+                continue;
+            }
+            if (button[i] != button[tmp]) {
+                return;
+            }
+            button[i] = button[i] == 1 ? 0 : 1;
+            button[tmp] = button[tmp] == 1 ? 0 : 1;
+        }
+    }
+}
