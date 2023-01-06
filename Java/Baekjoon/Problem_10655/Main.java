07d732c348e0749f42333a63607b77d00a40efd3
Ahhhhhhyeong <dkdud081@gmail.com>
2023-01-07 00:43:54 +0900

    10655 solved

--- Java/Baekjoon/Problem_10655/Main.java
+++ Java/Baekjoon/Problem_10655/Main.java
@@ -1,7 +1,41 @@
 package Java.Baekjoon.Problem_10655;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Main {
-    public static void main(String[] args) {
+    static int N;
+    static int[][] arr;
+    
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in)); //선언   
         
+        N = Integer.parseInt(bf.readLine());
+        arr = new int[N][2];
+        int distance = 0, min = Integer.MAX_VALUE; 
+        
+        StringTokenizer st;  
+        for(int i=0; i<N; i++){
+            st = new StringTokenizer(bf.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+            if( i >= 1){
+                distance += Math.abs(arr[i][0] - arr[i-1][0]) +  Math.abs(arr[i][1] - arr[i-1][1]);
+            }    
+        }
+
+        for(int i=1; i<N-1; i++){
+            int original = Math.abs(arr[i][0] - arr[i-1][0]) 
+                        +  Math.abs(arr[i][1] - arr[i-1][1])
+                        +  Math.abs(arr[i][0] - arr[i+1][0])
+                        +  Math.abs(arr[i][1] - arr[i+1][1]); // 건너뛰지 않는 경우 길이
+            int skip = Math.abs(arr[i-1][0] - arr[i+1][0])
+                    + Math.abs(arr[i-1][1] - arr[i+1][1]); //건너뛰는 경우의 길이
+            min =  Math.min(min, distance - original + skip);
+        }
+        System.out.println(min);
     }
 }
