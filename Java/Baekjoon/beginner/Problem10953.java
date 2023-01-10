b026f992fc1718bee40b9c08211d7afd8373f914
Ahhhhhhyeong <dkdud081@gmail.com>
2023-01-10 23:14:56 +0900

    10953 - A+B 6

---
+++ Java/Baekjoon/beginner/Problem10953.java
@@ -0,0 +1,16 @@
+package Java.Baekjoon.beginner;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Problem10953 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(bf.readLine());
+        for(int i=0; i<T; i++){
+            String str = bf.readLine();
+            System.out.println(Integer.parseInt(str.split(",")[0])+Integer.parseInt(str.split(",")[1]));
+        }
+    }
+}
