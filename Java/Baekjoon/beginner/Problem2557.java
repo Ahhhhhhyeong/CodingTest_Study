8e49ebbc2ba773ae8c60870afd55e0820964b51e
Ahhhhhhyeong <dkdud081@gmail.com>
2023-01-10 22:51:49 +0900

    2557 - hello world

---
+++ Java/Baekjoon/beginner/Problem2557.java
@@ -0,0 +1,10 @@
+package Java.Baekjoon.beginner;
+
+/**
+ * Problem2557
+ */
+public class Problem2557 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
