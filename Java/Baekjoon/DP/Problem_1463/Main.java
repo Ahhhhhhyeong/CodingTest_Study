36ee9acd19f72e3cb327c778efb0d13c9be60be8
Ahhhhhhyeong <dkdud081@gmail.com>
2023-01-18 19:35:40 +0900

    1463 - 1로 만들기

---
+++ Java/Baekjoon/DP/Problem_1463/Main.java
@@ -0,0 +1,26 @@
+package Java.Baekjoon.DP.Problem_1463;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in)); // 선언
+        int n = Integer.parseInt(bf.readLine());
+
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 0;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + 1;
+            if (i % 2 == 0)
+                dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+            if (i % 3 == 0)
+                dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+        }
+        System.out.println(dp[n]);
+    }
+
+}
