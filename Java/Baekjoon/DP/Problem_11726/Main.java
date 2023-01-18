e0853ce097b9c40c5a39cdf0f25b73184fcf4cf4
Ahhhhhhyeong <dkdud081@gmail.com>
2023-01-18 20:21:09 +0900

    11726 - 2×n 타일링

--- Java/Baekjoon/DP/Problem_11726/Main.java
+++ Java/Baekjoon/DP/Problem_11726/Main.java
@@ -9,13 +9,11 @@ public class Main {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in)); // 선언
         int n = Integer.parseInt(bf.readLine());
         int[] memo = new int[1001];
-        memo[0] = 1;
         memo[1] = 1;
         memo[2] = 2;
-        memo[3] = 3;
-        for (int i = 4; i <= n; i++) {
-            memo[i] = memo[i - 1] + memo[i - 2];
+        for (int i = 3; i <= n; i++) {
+            memo[i] = (memo[i - 1] + memo[i - 2]) % 10007;
         }
-        System.out.println(memo[n] % 10007);
+        System.out.println(memo[n]);
     }
 }
